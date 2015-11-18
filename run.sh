7cfb606c8d0a5ab61e7e30d391f752cb75d59515
Jason Xiong <jason.j.xiong@vanderbilt.edu>
2015-11-17 21:08:23 -0600

    fixed script

--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
-ocaml unix.cma main.ml
+#!/bin/bash
+if [ "$#" = "0" ]; then
+  corebuild main.native
+  ./main.native
+elif [ "$1" = "token" ]; then
+  corebuild main_test_token.native
+  ./main_test_token.native
+fi
+
+
