9747f5d16438fb7d5d785c408a0238056b9bad45
Jason Xiong <jason.j.xiong@vanderbilt.edu>
2015-11-22 16:42:57 -0600

    deleted useless methods, added quoted arguments to lexer

--- exec.ml
+++ exec.ml
@@ -3,40 +3,11 @@ open Unix
 
 open Bash
 
-let ls args =
-  Array.iter (fun v -> print_string (v^" ")) (Sys.readdir (Sys.getcwd ()));
-  print_newline ();
-  true;;
-
 let cd args =
   try
     Sys.chdir (List.nth args 1);
     true
   with Sys_error s -> print_endline s; false;;
-
-let sleep args =
-  if List.length args < 2
-  then (print_endline "sleep needs time param"; false)
-  else
-    try
-      let t = int_of_string (List.nth args 1) in
-      (Unix.sleep t; true)
-    with Failure _ -> print_endline "sleep needs int param"; false;;
-
-let echo args =
-  List.iter (fun v -> print_string (v^" ")) (List.tl args); 
-  print_newline (); 
-  true;;
-
-let cat args =
-  List.iter (fun v ->
-    let ic = open_in v in
-    try while true; do
-      print_endline (input_line ic)
-    done;
-    with End_of_file -> close_in ic) (List.tl args);
-  true;;
-
 
 let get_command_name args =
   match args with
@@ -58,7 +29,7 @@ let rec run_command comm bg =
   | `Command args ->
       let name = get_command_name args in
       if name = "" then true
-      else if name = "exit" then true
+      else if name = "exit" then exit 0
       else if name = "cd" then cd args
       else exec_command args bg
   | `Background comm' -> 
--- lexer.mll
+++ lexer.mll
@@ -8,6 +8,7 @@ exception SyntaxError of string
 let white = [' ' '\t']+
 let newline = '\r' | '\n' | "\r\n"
 let arg = ['a'-'z' 'A'-'Z' '_' '-' '0'-'9' '.' '/']+
+let quoted_arg = '"'_*'"'
 let semicolon = ';'
 let andthen = "&&"
 let orthen = "||"
@@ -19,6 +20,10 @@ rule read =
   parse
   | white    { read lexbuf }
   | newline { read lexbuf }
+  | quoted_arg { 
+      let s = Lexing.lexeme lexbuf in 
+      ARG (String.sub s 1 ((String.length s) - 2)) 
+    }
   | arg      { ARG (Lexing.lexeme lexbuf)}
   | semicolon { SEMICOLON }
   | andthen { AND }
