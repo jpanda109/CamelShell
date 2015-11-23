3ad7e6640da7e71df1a5478cf63198bd0ea30b86
Jason Xiong <jason.j.xiong@vanderbilt.edu>
2015-11-23 16:31:13 -0600

    changed out and in descriptors

--- exec.ml
+++ exec.ml
@@ -14,10 +14,10 @@ let get_command_name args =
   | [] -> ""
   | hd::_ -> hd;;
 
-let exec_command args bg outc =
+let exec_command args bg ind outd =
   match Unix.fork() with
   | 0 -> begin
-    Unix.dup2 (Unix.descr_of_out_channel outc) Unix.stdout;
+    Unix.dup2 outd Unix.stdout;
     execvp (List.nth args 0) (Array.of_list args)
   end
   | pid ->
@@ -27,27 +27,27 @@ let exec_command args bg outc =
       | WEXITED num -> num = 0
       | _ -> false;;
 
-let rec run_command comm bg outc =
+let rec run_command comm bg ind outd =
   match comm with
   | `Command args ->
       let name = get_command_name args in
       if name = "" then true
       else if name = "exit" then exit 0
       else if name = "cd" then cd args
-      else exec_command args bg outc
+      else exec_command args bg ind outd
   | `Background comm' -> 
-      run_command comm' true outc
+      run_command comm' true ind outd
   | `Redirect (c, arg) ->
-      run_command c bg (open_out arg)
+      run_command c bg ind (Unix.descr_of_out_channel (open_out arg))
   | `And (c1, c2) ->
-      run_command c1 bg outc && run_command c2 bg outc
+      run_command c1 bg ind outd && run_command c2 bg ind outd
   | `Or (c1, c2) ->
-      run_command c1 bg outc || run_command c2 bg outc
+      run_command c1 bg ind outd || run_command c2 bg ind outd
   | _ -> false;;
 
 
 let rec run_commands commands =
   match commands with
   | [] -> ()
-  | hd::tl -> ignore (run_command hd false (Unix.out_channel_of_descr Unix.stdout)); run_commands tl;;
+  | hd::tl -> ignore (run_command hd false Unix.stdin Unix.stdout); run_commands tl;;
 
